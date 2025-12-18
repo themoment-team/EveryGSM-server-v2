295c5ee9af16a24a462d19809315366533730f70
snowykte0426 <snowykte0426@naver.com>
2025-12-18 20:27:31 +0900

    init: Spring Boot 프로젝트 설정

---
+++ src/test/java/team/themoment/everygsm/server/v2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package team.themoment.everygsm.server.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
