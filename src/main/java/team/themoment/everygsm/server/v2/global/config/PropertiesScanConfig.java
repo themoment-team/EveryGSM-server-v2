adcd6a933ffa0cca1bfbfde209476edd0d95eaa5
hongjm0912~ <s25036@gsm.hs.kr>
2025-12-22 09:15:35 +0900

    feat(develop): 초기세팅 프로퍼티 클래스 정의

---
+++ src/main/java/team/themoment/everygsm/server/v2/global/config/PropertiesScanConfig.java
@@ -0,0 +1,9 @@
+package team.themoment.everygsm.server.v2.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = {})
+public class PropertiesScanConfig {
+}
