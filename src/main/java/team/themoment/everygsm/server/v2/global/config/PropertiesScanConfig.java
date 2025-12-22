37019b33e3cb2148adb40130e55f762dd4d588a9
hongjm0912~ <s25036@gsm.hs.kr>
2025-12-22 13:13:36 +0900

    refactor(develop): 프로퍼티 탐색 위치 설정

--- src/main/java/team/themoment/everygsm/server/v2/global/config/PropertiesScanConfig.java
+++ src/main/java/team/themoment/everygsm/server/v2/global/config/PropertiesScanConfig.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationPropertiesScan(basePackages = {})
+@ConfigurationPropertiesScan(basePackages = {"team.themoment.everygsm.server.v2.global.security.data"})
 public class PropertiesScanConfig {
 }
