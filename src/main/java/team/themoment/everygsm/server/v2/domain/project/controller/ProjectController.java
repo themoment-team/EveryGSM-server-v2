a39a703a2339f0e829b1e6838f4f9d9fa5010bb8
hongjm0912~ <s25036@gsm.hs.kr>
2026-01-20 23:36:57 +0900

    refactor(project): 프로젝트 등록 기능 컨트롤러 반환값 변경

--- src/main/java/team/themoment/everygsm/server/v2/domain/project/controller/ProjectController.java
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/controller/ProjectController.java
@@ -1,14 +1,15 @@
 package team.themoment.everygsm.server.v2.domain.project.controller;
 
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team.themoment.everygsm.server.v2.domain.project.dto.requset.RegisterProjectReqDto;
+import team.themoment.everygsm.server.v2.domain.project.dto.request.RegisterProjectReqDto;
+import team.themoment.everygsm.server.v2.domain.project.dto.response.RegisterProjectResDto;
 import team.themoment.everygsm.server.v2.domain.project.service.RegisterProjectService;
-import team.themoment.everygsm.server.v2.global.common.response.CommonApiResponse;
 
 @RestController
 @RequestMapping("/api/v1/project")
@@ -17,9 +18,8 @@ public class ProjectController {
     private final RegisterProjectService registerProjectService;
 
     @PostMapping("/registration")
-    public CommonApiResponse register(@RequestBody RegisterProjectReqDto reqDto){
+    public RegisterProjectResDto register(@RequestBody @Valid RegisterProjectReqDto reqDto){
         Long userId = 1L;
-        registerProjectService.execute(userId, reqDto);
-        return CommonApiResponse.success("등록되었습니다.");
+        return registerProjectService.execute(userId, reqDto);
     }
 }
