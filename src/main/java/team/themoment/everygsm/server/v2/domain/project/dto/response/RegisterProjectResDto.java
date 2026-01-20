e383d7577fbfff321a590cbadea13182aaf8fe17
hongjm0912~ <s25036@gsm.hs.kr>
2026-01-20 23:15:06 +0900

    feat(project): 프로젝트 등록 응답 dto 추가

---
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/dto/response/RegisterProjectResDto.java
@@ -0,0 +1,24 @@
+package team.themoment.everygsm.server.v2.domain.project.dto.response;
+
+import team.themoment.everygsm.server.v2.domain.project.dto.common.RepositoryDto;
+import team.themoment.everygsm.server.v2.domain.project.dto.common.TechStackDto;
+import team.themoment.everygsm.server.v2.domain.project.entity.constant.Status;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record RegisterProjectResDto(
+        Long projectId,
+        String logo,
+        String title,
+        String affiliation,
+        String description,
+        String prodUrl,
+        Status status,
+        String reason,
+        LocalDateTime createdAt,
+        List<TechStackDto> techStack,
+        List<RepositoryDto> repository,
+        boolean liked
+) {
+}
