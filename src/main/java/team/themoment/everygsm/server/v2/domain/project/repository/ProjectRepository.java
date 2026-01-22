3e28c0f8aa5f842bab0d8526032fb0dfe0f88759
hongjm0912~ <s25036@gsm.hs.kr>
2026-01-22 22:54:53 +0900

    refactor(project): 미사용 쿼리메서드 제거

--- src/main/java/team/themoment/everygsm/server/v2/domain/project/repository/ProjectRepository.java
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/repository/ProjectRepository.java
@@ -1,32 +1,13 @@
 package team.themoment.everygsm.server.v2.domain.project.repository;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import team.themoment.everygsm.server.v2.domain.project.entity.ProjectJpaEntity;
 import team.themoment.everygsm.server.v2.domain.project.entity.constant.Status;
 
 public interface ProjectRepository extends JpaRepository<ProjectJpaEntity, Long> {
-
-    @Query("""
-                SELECT r
-                FROM ProjectJpaEntity p
-                JOIN p.repoUrls r
-                WHERE p.id = :projectId
-            """)
-    Set<String> findRepoUrlsByProjectId(@Param("projectId") Long projectId);
-
-    @Query("""
-                SELECT s
-                FROM ProjectJpaEntity p
-                JOIN p.stackNames s
-                WHERE p.id = :projectId
-            """)
-    Set<String> findStackNamesByProjectId(@Param("projectId") Long projectId);
 
     List<ProjectJpaEntity> findByStatus(Status status);
 }
