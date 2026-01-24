a61ba4281f0a49205f3309d1bd20c38778b3d073
hongjm0912~ <s25036@gsm.hs.kr>
2026-01-24 17:30:50 +0900

    refactor(project): 네이밍 수정

--- src/main/java/team/themoment/everygsm/server/v2/domain/project/dto/response/QueryProjectResDto.java
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/dto/response/QueryProjectResDto.java
@@ -2,5 +2,5 @@ package team.themoment.everygsm.server.v2.domain.project.dto.response;
 
 import java.util.List;
 
-public record ViewProjectResDto(List<ProjectResDto> projects) {
+public record QueryProjectResDto(List<ProjectResDto> projects) {
 }
