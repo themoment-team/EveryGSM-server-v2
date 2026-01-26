701fb8ecb2c3120ac0001b261f6c5810e61b93e2
hongjm0912~ <s25036@gsm.hs.kr>
2026-01-26 21:27:18 +0900

    feat(admin): 어드민 프로젝트 조회 기능 개발

---
+++ src/main/java/team/themoment/everygsm/server/v2/domain/admin/service/AdminQueryProjectService.java
@@ -0,0 +1,36 @@
+package team.themoment.everygsm.server.v2.domain.admin.service;
+
+import static team.themoment.everygsm.server.v2.domain.project.entity.constant.Status.PENDING;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import team.themoment.everygsm.server.v2.domain.project.dto.response.ProjectResDto;
+import team.themoment.everygsm.server.v2.domain.project.dto.response.QueryProjectResDto;
+import team.themoment.everygsm.server.v2.domain.project.entity.ProjectJpaEntity;
+import team.themoment.everygsm.server.v2.domain.project.mapper.ProjectMapper;
+import team.themoment.everygsm.server.v2.domain.project.repository.ProjectRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AdminQueryProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    @Transactional
+    public QueryProjectResDto execute() {
+        return buildAdminQueryResDto();
+    }
+
+    private QueryProjectResDto buildAdminQueryResDto() {
+        List<ProjectJpaEntity> projects = projectRepository.findByStatus(PENDING);
+        List<ProjectResDto> res = projects.stream().map(projectMapper::toRes).toList();
+
+        return new QueryProjectResDto(res);
+    }
+
+}
