50f57ae7a7b2af4981b9abdef391d42439cd6e36
snowykte0426 <snowykte0426@naver.com>
2026-03-03 09:59:19 +0900

    feat(global): DataGSM OpenAPI SDK 및 스케줄러 설정 추가

---
+++ src/main/java/team/themoment/everygsm/server/v2/global/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package team.themoment.everygsm.server.v2.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
