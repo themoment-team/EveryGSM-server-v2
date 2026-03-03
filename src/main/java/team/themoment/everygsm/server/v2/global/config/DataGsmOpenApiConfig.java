4e0e352d1becd30232f53c2592b47a8f685529a4
snowykte0426 <snowykte0426@naver.com>
2026-03-03 10:28:17 +0900

    update(global): Spotless 자동 포맷팅 적용

--- src/main/java/team/themoment/everygsm/server/v2/global/config/DataGsmOpenApiConfig.java
+++ src/main/java/team/themoment/everygsm/server/v2/global/config/DataGsmOpenApiConfig.java
@@ -3,6 +3,7 @@ package team.themoment.everygsm.server.v2.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import team.themoment.datagsm.sdk.openapi.DataGsmOpenApiClient;
 
 @Configuration
