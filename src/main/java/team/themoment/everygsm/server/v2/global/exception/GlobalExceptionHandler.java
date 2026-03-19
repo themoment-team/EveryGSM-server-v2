01e04feb51e4dea8fb7d3cc065a3d305a3eb77e1
snowykte0426 <snowykte0426@naver.com>
2026-03-19 20:31:12 +0900

    add(global): 전역 예외 처리를 직접 정의

---
+++ src/main/java/team/themoment/everygsm/server/v2/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package team.themoment.everygsm.server.v2.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import team.themoment.everygsm.server.v2.global.exception.error.ExpectedException;
+import team.themoment.sdk.response.CommonApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ExpectedException.class)
+    public ResponseEntity handleExpectedException(ExpectedException e) {
+        return ResponseEntity.status(e.getStatusCode())
+                .body(CommonApiResponse.error(e.getMessage(), e.getStatusCode()));
+    }
+}
