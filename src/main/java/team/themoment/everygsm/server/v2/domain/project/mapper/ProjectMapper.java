270eafae1eda2bed8d41da4df416471e7c12e704
hongjm0912~ <s25036@gsm.hs.kr>
2026-03-29 16:41:02 +0900

    update(project): Mapper에서 응답 제작 시 logo 필드에 PresignerUrl을 담아 반환 하도록 수정

--- src/main/java/team/themoment/everygsm/server/v2/domain/project/mapper/ProjectMapper.java
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/mapper/ProjectMapper.java
@@ -1,23 +1,35 @@
 package team.themoment.everygsm.server.v2.domain.project.mapper;
 
+import java.time.Duration;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import team.themoment.everygsm.server.v2.domain.project.dto.common.RepositoryDto;
 import team.themoment.everygsm.server.v2.domain.project.dto.common.TechStackDto;
 import team.themoment.everygsm.server.v2.domain.project.dto.response.ProjectResDto;
 import team.themoment.everygsm.server.v2.domain.project.entity.ProjectJpaEntity;
 
 @Component
+@RequiredArgsConstructor
 public class ProjectMapper {
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
 
     public ProjectResDto toRes(ProjectJpaEntity project, boolean liked) {
         List<TechStackDto> techStacks = extractTechStacks(project);
         List<RepositoryDto> repositories = extractRepositories(project);
 
         return new ProjectResDto(project.getId(),
-                project.getLogo(),
+                generatePresignedUrl(project.getLogo()),
                 project.getTitle(),
                 project.getAffiliation(),
                 project.getDescription(),
@@ -28,6 +40,15 @@ public class ProjectMapper {
                 techStacks,
                 repositories,
                 liked);
+    }
+
+    private String generatePresignedUrl(String key) {
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofHours(1))
+                .getObjectRequest(req -> req.bucket(bucket).key(key))
+                .build();
+        PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(presignRequest);
+        return presignedRequest.url().toString();
     }
 
     public List<TechStackDto> extractTechStacks(ProjectJpaEntity project) {
