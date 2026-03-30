77357ab5288c3e249d0da85d51bdabc4cb1adbf6
zaman <roblery128@gmail.com>
2026-03-30 21:55:08 +0900

    refactor(project): 사용하지 않는 메서드 삭제

--- src/main/java/team/themoment/everygsm/server/v2/domain/project/repository/custom/ProjectRepositoryCustom.java
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/repository/custom/ProjectRepositoryCustom.java
@@ -9,7 +9,6 @@ import team.themoment.everygsm.server.v2.domain.project.entity.constant.Status;
 
 public interface ProjectRepositoryCustom {
     List<ProjectJpaEntity> findLikedProjectsByUserId(Long userId);
-    List<ProjectJpaEntity> findRegisteredProjectsByUserId(Long userId);
     List<ProjectJpaEntity> findByUserIdAndStatus(Long userId, Status status);
     List<ProjectJpaEntity> findAllByStatusWithCollections(Status status);
     void markUnseenProjectsAsInactive(Set<Long> seenIds);
