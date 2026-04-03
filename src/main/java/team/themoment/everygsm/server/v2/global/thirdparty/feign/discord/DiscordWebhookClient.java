f9fbdb3626c957e4a426f9a063a29c859a377351
zaman <roblery128@gmail.com>
2026-04-03 14:27:08 +0900

    refactor(global): thirdparty 패키지를 global 패키지로 이동

--- src/main/java/team/themoment/everygsm/server/v2/global/thirdparty/feign/discord/DiscordWebhookClient.java
+++ src/main/java/team/themoment/everygsm/server/v2/global/thirdparty/feign/discord/DiscordWebhookClient.java
@@ -1,10 +1,10 @@
-package team.themoment.everygsm.server.v2.thirdparty.feign.discord;
+package team.themoment.everygsm.server.v2.global.thirdparty.feign.discord;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import team.themoment.everygsm.server.v2.thirdparty.feign.discord.dto.DiscordWebhookPayload;
+import team.themoment.everygsm.server.v2.global.thirdparty.feign.discord.dto.DiscordWebhookPayload;
 
 @FeignClient(name = "discordWebhook", url = "${spring.cloud.discord.webhook.url}")
 public interface DiscordWebhookClient {
