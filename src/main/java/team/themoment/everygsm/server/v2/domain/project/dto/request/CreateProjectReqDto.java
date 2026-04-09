90bac33c680a9a567bf3cae495c4a0e54de5fade
zaman <roblery128@gmail.com>
2026-04-09 17:54:27 +0900

    refactor(project): 프로젝트 생성 시 repoName을 받지 않도록 수정

--- src/main/java/team/themoment/everygsm/server/v2/domain/project/dto/request/CreateProjectReqDto.java
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/dto/request/CreateProjectReqDto.java
@@ -7,10 +7,9 @@ import org.hibernate.validator.constraints.URL;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import team.themoment.everygsm.server.v2.domain.project.dto.common.RepositoryDto;
 import team.themoment.everygsm.server.v2.domain.project.dto.common.TechStackDto;
 
 public record CreateProjectReqDto(@NotBlank String logo, @NotBlank String title, @NotBlank String affiliation,
         @NotBlank @Size(max = 200) String description, @URL String prodUrl, @NotNull List<TechStackDto> techStack,
-        @NotNull List<RepositoryDto> repository) {
+        @NotNull List<String> repository) {
 }
