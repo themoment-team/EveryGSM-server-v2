81a841d16125410795b6ccdad1585cc3311e4a22
zaman <roblery128@gmail.com>
2026-04-09 18:20:00 +0900

    refactor(project): repository의 url은 nullable하고, url 형식으로 받도록 수정

--- src/main/java/team/themoment/everygsm/server/v2/domain/project/dto/request/CreateProjectReqDto.java
+++ src/main/java/team/themoment/everygsm/server/v2/domain/project/dto/request/CreateProjectReqDto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.URL;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,5 +12,5 @@ import team.themoment.everygsm.server.v2.domain.project.dto.common.TechStackDto;
 
 public record CreateProjectReqDto(@NotBlank String logo, @NotBlank String title, @NotBlank String affiliation,
         @NotBlank @Size(max = 200) String description, @URL String prodUrl, @NotNull List<TechStackDto> techStack,
-        @NotNull List<String> repository) {
+        @Valid List<@URL String> repository) {
 }
